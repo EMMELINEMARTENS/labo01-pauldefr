67bd0b08567e33b0a465818ae447f6f8cdb33e08
Paulien De Fraine <paulien.defraine@student.arteveldehs.be>
2019-10-01 21:18:08 +0200

    initial commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,18 +6,18 @@
 /* eslint-disable-next-line no-plusplus */
 for (let i = 0; i < 10; i++) {
     const users = () => {
-      // getting data
-      fetch('https://randomuser.me/api/?results=10')
-        .then((response) => response.json())
-        .then((data) => data.results)
-        .catch((err) => err.message);
+        // getting data
+        fetch('https://randomuser.me/api/?results=10')
+            .then((response) => response.json())
+            .then((data) => data.results)
+            .catch((err) => err.message);
     };
-  
+
     const show = () => {
-      // show users
+        // show users
     };
-  }
-  
-  users();
-  
-  // localstorage
+}
+
+users();
+
+// localstorage
