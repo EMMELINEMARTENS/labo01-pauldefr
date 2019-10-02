7609e5dae9f354245a81c77b252fa894b293685f
Paulien De Fraine <paulien.defraine@student.arteveldehs.be>
2019-10-02 13:59:10 +0200

    app update

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,29 +5,27 @@ import '../styles/main.css';
 
 /* eslint-disable-next-line no-plusplus */
 // getting data
-
 //verbeteringen Emmeline
- 
 const fetchfunc = () => {
-    fetch('https://randomuser.me/api/?results=10')
-  .then((response) => response.json())
-  .then((data) =>  {
+  fetch('https://randomuser.me/api/?results=10')
+    .then((response) => response.json())
+    .then((data) =>  {
       console.log(data);
       UsersCreated(data.results);
-    };
-  .catch((err) => console.log(error.message));
+    })
+    .catch((err) => console.log(error.message));
 };
 
-for (let i = 0; i < users.length; i++) {
-  const users = () => {
-
-  };
-
-    const show = () => {
-        // show users
-    };
+function UsersCreated(users) {
+  for (let i = 0; i < users.lengt; i++) {
+    const user = users[i];
+  }
 }
 
-users();
-
 // localstorage
+
+const saveInStorage = (users) => {
+  const storageUsers = JSON.parse(localStorage.getItem('users'));
+};
+
+localStorage.setItem('users', JSON.stringify(storageUsers.concat(users)));
